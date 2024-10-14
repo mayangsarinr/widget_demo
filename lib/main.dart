d1fdc6598a383071d5ac618741dc231138ec4723
mayangsarinr <mayangsarinr_2327240064@mhs.mdp.ac.id>
2024-10-14 13:12:43 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Widget Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Widget Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.blue,
+            height: 200,
+            padding: EdgeInsets.all(16),
+            margin: EdgeInsets.all(16),
+            child: Text(
+              "Ini adalah contoh penggunaan Container",
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
